a5a6f770b1866e73040e1febff458bfefca443ad
芸芸众生 <xingyuelongchen@qq.com>
2021-11-11 14:51:27 +0800

    commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+var app = express();
+
+app.use(express.static("docs"));
+app.use('/api', createProxyMiddleware({
+  target: 'http://159.75.35.27:6899/api', // 服务器api地址目录
+  changeOrigin: true,
+  // pathRewrite: {
+  //   "^/api": "/"
+  // }
+}));
+
+
+app.listen(80, function () { // 代理接口
+  console.log('代理接口启动成功,端口:http://192.168.0.101:80');
+});
